b58d47bfd954a8b605cfd80a597dbfd193c2bfa3
codertray <blindguy1992@gmail.com>
2024-02-20 09:06:21 -0700

    initial commit most core functionality complete

---
+++ backend/DB.js
@@ -0,0 +1,141 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "../.env"
+});
+
+const pool = new pg.Pool({
+    database: 'blog',
+    host: "localhost",
+    port: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+});
+
+const data = {
+    async getRoutes() {
+        try {
+            const result = await pool.query("SELECT id, route FROM posts;");
+            return {
+                success: true,
+                data: result.rows,
+            };
+        } catch (e) {
+            return {
+                success: false,
+                data: e.stack
+            };
+        }
+    },
+
+    async getPosts() {
+        try {
+            const result = await pool.query("SELECT title, route, date, blerb FROM posts;");
+            return {
+                success: true,
+                data: result.rows
+            };
+        } catch (e) {
+            return {
+                success: false,
+                data: e.stack
+            };
+        }
+    },
+
+    async listPosts() {
+        try {
+            const result = await pool.query("SELECT id, title, route, date FROM posts;");
+            return {
+                success: true,
+                data: result.rows
+            };
+        } catch (e) {
+            return {
+                success: false,
+                data: e.stack
+            };
+        }
+    },
+
+
+    async filterTopic(topic) {
+        try {
+            const result = await pool.query("SELECT title, route, date, blerb FROM posts WHERE topic = $1", [topic]);
+            return {
+                success: true,
+                data: result.rows
+            };
+        } catch (e) {
+            return {
+                success: false,
+                data: e.stack
+            };
+        }
+    },
+
+    async displayPost(id) {
+        try {
+            const result = await pool.query("SELECT title, image, content FROM posts WHERE id = $1;", [id]);
+            return {
+                success: true,
+                data: result.rows[0]
+            };
+        } catch (e) {
+            return {
+                success: false,
+                data: e.stack
+            };
+        }
+    },
+
+    async addPost(title, route, date, image, content, blerb) {
+        try {
+            await pool.query("INSERT INTO posts (title, route, date, image, content, blerb) VALUES ($1, $2, $3, $4, $5, $6);",
+            [title, route, date, image, content, blerb]);
+            return {
+                success: true,
+                data: "query returned successfully",
+            };
+        } catch (e) {
+            return {
+                success: false,
+                data: `an error has occurred. ${e.stack}`,
+            };
+        }
+    },
+
+    async updatePost(id, title, route, image, content, blerb) {
+        try {
+            await pool.query("UPDATE posts SET title = $2, route = $3, image = $4, content = $5, blerb = $6 WHERE id = $1",
+            [id, title, route, image, content, blerb]);
+            return {
+                success: true,
+                data: "query returned successfully",
+            };
+        } catch (e) {
+            return {
+                success: false,
+                data: `an error has occurred. ${e.stack}`,
+            };
+        }
+    },
+
+    async deletePost(id) {
+        try {
+            await pool.query("DELETE FROM posts WHERE id = $1", [id]);
+            return {
+                success: true,
+                data: "query returned successfully",
+            };
+        } catch (e) {
+            return {
+                success: false,
+                data: `an error has occurred. ${e.stack}`,
+            };
+        }
+    }
+};
+
+export default data;
---
+++ backend/index.js
@@ -0,0 +1,218 @@
+import express, { Router }  from "express"
+import fs from "fs"
+import data from "./DB.js"
+import bcrypt from "bcryptjs"
+import env from "dotenv"
+import session from "express-session";
+import passport from "passport";
+import local from "passport-local"
+
+
+const app = express();
+let postRoutes = {};
+const port = 3000;
+
+env.config({
+    path: "../.env"
+})
+
+app.use(express.static("../public"));
+app.use(express.urlencoded({extended: true}));
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: true   
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session())
+app.use(async (req, res, next) => {
+    const result = await data.getRoutes()
+    !result.success && res.redirect("/blog/500");
+    res.locals.postRoutes = result.data;
+    next();
+});
+
+app.get("/blog", async (req, res) => {
+    const result = await data.getPosts();
+    !result.success ? () => {
+        res.redirect("/blog/500");
+        console.log(result.data);
+    } : res.render("../views/home.ejs", {data: result.data})
+});
+
+app.get("/blog/about", (req, res) => {
+    res.render("../views/about.ejs");
+});
+
+app.get("/blog/404", (req, res) => {
+    res.render("../views/404.ejs");
+});
+
+app.get("/blog/500", (req, res) => {
+    res.render("../views/500");
+});
+
+app.get("/blog/admin", (req, res ) => {
+    req.isAuthenticated() && res.redirect("/blog/admin/panel")
+    res.render("../views/login.ejs");
+});
+
+app.post("/blog/login", passport.authenticate("local", {
+    failureRedirect: "/blog/admin"
+}), (req, res) => {
+    res.redirect("/blog/admin/panel")
+});
+
+app.get("/blog/admin/panel", (req, res, next) => {
+    req.isAuthenticated() ? checkPosts(req, res, next) : res.redirect("/blog/admin");
+});
+async function checkPosts(req, res, next) {
+    try {
+        const result = await data.listPosts();
+        if (!result.success) {
+            console.log("Error:", result.data);
+            res.redirect("/blog/500");
+        } else {
+            res.render("../views/panel.ejs", { data: result.data });
+        }
+    } catch (err) {
+        console.error("Error:", err);
+        res.redirect("/blog/500");
+    }
+}
+
+app.get("/blog/admin/write", (req, res) => {
+    res.render("../views/form.ejs");
+});
+
+app.get("/blog/admin/:post.edit", async (req, res) => {
+    post = req.params.post;
+    const postRoutes = res.locals.postRoutes
+    foundPost = postRoutes.find((postRoute) => (postRoute === post));
+    const result = await data.displayPost(foundPost.id);
+    !result.success ? () => {
+        res.redirect("/blog/404");
+        console.log(result.data);
+    } : res.render("../views/form.ejs", {data: result.data});
+});
+
+app.post("/blog/admin/delete", async (req, res) => {
+    const postid = req.body.id;
+    try {
+        const result = await data.deletePost(postid);
+        if (!result.success) {
+            console.error("Error deleting post:", result.data);
+            res.redirect("/blog/500");
+        } else {
+            res.redirect("/blog/admin/panel");
+        }
+    } catch (err) {
+        console.error("Error:", err);
+        res.redirect("/blog/500");
+    }
+});
+
+app.post("/blog/admin/post", async (req, res) => {
+    const { title, image, content, blerb, route } = req.body;
+    const date = new Date().toISOString(); // Use ISO string for date
+    const imagePath = `../public/assets/${image.filename}`;
+    fs.writeFile(magePath, image.data, async (err) => {
+        if (err) {
+            console.error("Error writing image:", err);
+            res.redirect("back");
+        } else {
+            const result = await data.addPost(title, route, date, imagePath, content, blerb);
+            if (!result.success) {
+                console.error("Error adding post:", result.data);
+                res.redirect("back");
+            } else {
+                res.redirect("/blog/admin/panel");
+            }
+        }
+    });
+});
+
+app.post("/blog/admin/update", async (req, res) => {
+    const { title, image, content, blerb } = req.body;
+    const imagePath = `../public/assets/${image.filename}`;
+    fs.writeFile(imagePath, image.data, async (err) => {
+        if (err) {
+            console.error("Error writing image:", err);
+            res.redirect("back");
+        } else {
+            const result = await data.updatePost(title, date, imagePath, content, blerb);
+            if (!result.success) {
+                console.error("Error adding post:", result.data);
+                res.redirect("back");
+            } else {
+                res.redirect("/blog/admin/panel");
+            }
+        }
+    });
+});
+
+app.get("/blog/:topic-posts", async (req, res) => {
+    const topic = req.params.topic;
+    const result = await data.filterTopic(topic);
+    !result.success ? () => {
+        res.redirect("/blog/500");
+        console.log(result.data)
+    } : res.render("../views/filter.ejs", {topic: topic, data: result.data});
+});
+
+app.get("/blog/:post", async (req, res) => {
+    const post = req.params.post;
+    const postRoutes = res.locals.postRoutes;
+    const foundPost = postRoutes.find(postRoute => postRoute.route === post);  
+    if (foundPost) {
+        const result = await data.displayPost(foundPost.id);
+        if (!result.success) {
+            console.error("Error displaying post:", result.data);
+            res.redirect("/blog/500");
+        } else {
+            res.render("../views/post.ejs", { data: result.data });
+        }
+    } else {
+        res.redirect("/blog/404");
+    }
+});
+
+passport.use(new local.Strategy(async function verify(id, password, done) {
+    try {
+        const hash = process.env.PASSWORD;
+        if(id !== process.env.ADMIN_ID) {
+            console.log("wrong id")
+            return done(null, false);
+        } else {
+            const result = await bcrypt.compare(password, hash);
+            if (result) {
+                console.log("success");
+                return done(null, { id: process.env.ADMIN_ID, password: password });
+            } else {
+                console.log("wrong password");
+                return done(null, false);
+            }
+        }
+    } catch (err) {
+        console.error("Error:", err);
+        return done(err);
+    }
+}));
+
+
+  
+
+passport.serializeUser(function(admin, cb) {
+    cb(null, admin);
+});
+
+passport.deserializeUser(function(admin, cb) {
+    cb(null, admin);
+});
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
+
